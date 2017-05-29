e0d264bbd8f445f047301ed36841d7b933139b1a
Alex <alomsimoy@gmail.com>
2017-05-29 18:42:05 +0200

    Main app core

---
+++ main.js
@@ -0,0 +1,20 @@
+var html = require('yo-yo');
+var root = document.body.appendChild(document.createElement('div'));
+
+var names = [
+  'Alex Piqueras',
+];
+
+var list = names.map(function (name) {
+  return html`<li>${name}</li>`;
+});
+
+html.update(
+  root,
+  html`<div>
+    <h2>Nombres</h2>
+    <ul>
+    ${list}
+    </ul>
+  </div>`
+);
